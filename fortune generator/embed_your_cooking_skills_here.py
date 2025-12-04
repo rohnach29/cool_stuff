666ec4090680f5f43b2125748fcea6b31ac829ae
rohnach29 <rohnach29@gmail.com>
2025-12-04 12:04:02 -0500

    Uncomment code blocks

--- fortune generator/embed_your_cooking_skills_here.py
+++ fortune generator/embed_your_cooking_skills_here.py
@@ -31,35 +31,37 @@ for i in range(0, len(text) - SEQ_LENGTH, STRIDE):
     next_letter.append(text[i + SEQ_LENGTH])
 #now with vector embeddings
 #for vector embeddings watch this vidoe https://www.youtube.com/watch?v=viZrOnJclY0
-# x = np.zeros((len(sentences), SEQ_LENGTH), dtype=np.int32)
-# y = np.zeros((len(sentences),), dtype=np.int32)
+x = np.zeros((len(sentences), SEQ_LENGTH), dtype=np.int32)
+y = np.zeros((len(sentences),), dtype=np.int32)
 
-# #now need to fill up x and y
-# for i in range(len(sentences)):
-#     for j in range(SEQ_LENGTH):
-#         #the 5th sentence
-#         x[i][j] = char_to_index[sentences[i][j]]
+#now need to fill up x and y
+for i in range(len(sentences)):
+    for j in range(SEQ_LENGTH):
+        #the 5th sentence
+        x[i][j] = char_to_index[sentences[i][j]]
 
-#     y[i] = char_to_index[next_letter[i]]
+    y[i] = char_to_index[next_letter[i]]
 
-# early_stop = EarlyStopping(
-#     monitor="val_loss",       # metric to watch
-#     patience=3,               # how many epochs to wait before stopping
-#     restore_best_weights=True # go back to the best epoch
-# )
+early_stop = EarlyStopping(
+    monitor="val_loss",       # metric to watch
+    patience=3,               # how many epochs to wait before stopping
+    restore_best_weights=True # go back to the best epoch
+)
 
-# model = Sequential()
-# model.add(layers.Embedding(input_dim=len(chars), output_dim=64, input_length=SEQ_LENGTH))  #64 dimesnions -> captures multiple overlapping uses of the character at once
-# #captures things like "a can be silent. a is a vowel, a can start a word, a is not punctuation" -> this is captured in 64 dimensions
-# #but why 64? general practice seems like -> 32 to 128 is general for character level
-# model.add(layers.LSTM(128))     #always need to specify input shape somewhere
-# model.add(layers.Dropout(0.3))
-# model.add(layers.Dense(len(chars), activation="softmax"))
+model = Sequential()
+model.add(layers.Embedding(input_dim=len(chars), output_dim=64, input_length=SEQ_LENGTH))  #64 dimesnions -> captures multiple overlapping uses of the character at once
+#captures things like "a can be silent. a is a vowel, a can start a word, a is not punctuation" -> this is captured in 64 dimensions
+#but why 64? general practice seems like -> 32 to 128 is general for character level
+model.add(layers.LSTM(128))     #always need to specify input shape somewhere
+model.add(layers.Dropout(0.3))
+model.add(layers.Dense(len(chars), activation="softmax"))
 
-# model.compile(optimizer="adam", loss="sparse_categorical_crossentropy")
-# model.fit(x, y, validation_split=0.1, batch_size=128, epochs=50, callbacks=[early_stop])
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy")
+model.fit(x, y, validation_split=0.1, batch_size=128, epochs=50, callbacks=[early_stop])
 
-# model.save("embedded_fortune.keras")
+model.save("embedded_fortune.keras")
+
+#to run this next part, comment out from "watch" to model.save
 
 model = tf.keras.models.load_model("embedded_fortune.keras")
 
--- fortune generator/you_will_be_a_good_cook.py
+++ fortune generator/you_will_be_a_good_cook.py
@@ -29,31 +29,33 @@ for i in range(0, len(text) - SEQ_LENGTH, STRIDE):
     sentences.append(text[i: i + SEQ_LENGTH]) #i = 2: 2 to 42 (so 2 to 41, which is 40)
     next_letter.append(text[i + SEQ_LENGTH])
 #let's try with one-hot vectors first
-# x = np.zeros((len(sentences), SEQ_LENGTH, len(chars)), dtype=np.float32)
-# y = np.zeros((len(sentences), len(chars)), dtype=np.float32)
+x = np.zeros((len(sentences), SEQ_LENGTH, len(chars)), dtype=np.float32)
+y = np.zeros((len(sentences), len(chars)), dtype=np.float32)
 
-# #now need to fill up x and y
-# for i in range(len(sentences)):
-#     for j in range(SEQ_LENGTH):
-#         x[i][j][char_to_index[sentences[i][j]]] = 1
+#now need to fill up x and y
+for i in range(len(sentences)):
+    for j in range(SEQ_LENGTH):
+        x[i][j][char_to_index[sentences[i][j]]] = 1
 
-#     y[i][char_to_index[next_letter[i]]] = 1
+    y[i][char_to_index[next_letter[i]]] = 1
 
-# early_stop = EarlyStopping(
-#     monitor="val_loss",       # metric to watch
-#     patience=3,               # how many epochs to wait before stopping
-#     restore_best_weights=True # go back to the best epoch
-# )
+early_stop = EarlyStopping(
+    monitor="val_loss",       # metric to watch
+    patience=3,               # how many epochs to wait before stopping
+    restore_best_weights=True # go back to the best epoch
+)
 
-# model = Sequential()
-# model.add(layers.LSTM(128, input_shape=(SEQ_LENGTH, len(chars))))       #always need to specify input shape somewhere. input_shape is (time dimensions, feature dimension)
-# model.add(layers.Dropout(0.3))
-# model.add(layers.Dense(len(chars), activation="softmax"))
+model = Sequential()
+model.add(layers.LSTM(128, input_shape=(SEQ_LENGTH, len(chars))))       #always need to specify input shape somewhere. input_shape is (time dimensions, feature dimension)
+model.add(layers.Dropout(0.3))
+model.add(layers.Dense(len(chars), activation="softmax"))
 
-# model.compile(optimizer="adam", loss="categorical_crossentropy")
-# model.fit(x, y, validation_split=0.1, batch_size=128, epochs=50, callbacks=[early_stop])
+model.compile(optimizer="adam", loss="categorical_crossentropy")
+model.fit(x, y, validation_split=0.1, batch_size=128, epochs=50, callbacks=[early_stop])
 
-# model.save("fortune.keras")
+model.save("fortune.keras")
+
+#to run this next part, comment out from "let's try" to model.save
 
 model = tf.keras.models.load_model("fortune.keras")
 
